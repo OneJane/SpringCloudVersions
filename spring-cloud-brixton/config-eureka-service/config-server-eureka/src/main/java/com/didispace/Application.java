510a41f447a9118ded615919b1bdf46a8a876366
OneJane <15806204096@163.com>
2019-07-03 11:49:26 +0800

    config 加入eureka配置中心 fix

--- spring-cloud-brixton/config-eureka-service/config-server-eureka/src/main/java/com/didispace/Application.java
+++ spring-cloud-brixton/config-eureka-service/config-server-eureka/src/main/java/com/didispace/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
