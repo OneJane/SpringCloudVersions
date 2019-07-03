54db52bfedc308908e5725251fb36e9751644cd1
OneJane <15806204096@163.com>
2019-07-03 20:26:13 +0800

    9.Finchley版-同一通道根据消息内容分发不同的消费逻辑

--- spring-cloud-finchley/stream-consumer-self/src/main/java/com/didispace/stream/TestController.java
+++ spring-cloud-finchley/stream-consumer-self/src/main/java/com/didispace/stream/TestController.java
@@ -2,11 +2,7 @@ package com.didispace.stream;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
